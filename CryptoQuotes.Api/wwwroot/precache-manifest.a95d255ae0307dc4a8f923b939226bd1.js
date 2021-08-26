e5d2ebbcb9a45e7a7868b01313f2d27bb1451ac5
Vacuum113 <dimentr12170@gmail.com>
2021-08-26 21:18:25 +0500

    + client added

---
+++ CryptoQuotes.Api/wwwroot/precache-manifest.a95d255ae0307dc4a8f923b939226bd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2206bc32f662a2b7e228214a5a4062",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a25c1110d18ba0165ce3",
+    "url": "/static/js/2.028e24ba.chunk.js"
+  },
+  {
+    "revision": "07c1906d94814638a78b7b7d6b918030",
+    "url": "/static/js/2.028e24ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8364740c5984be18b0ee",
+    "url": "/static/js/main.46629e38.chunk.js"
+  },
+  {
+    "revision": "a14b84851a7e5c3bc300",
+    "url": "/static/js/runtime-main.0cb75d20.js"
+  }
+]);
